21a3bd38c0059c320cb95eac018131f12374d544
Bianca M. <eng.biancagirardi@gmail.com>
2024-08-09 16:53:26 -0300

    Create bat
    Criação do arquivo batch para rodar as seções

---
+++ ANALYSIS/ANALISES_01/bat_para_rodar_secao.bat
@@ -0,0 +1,65 @@
+SET pasta_ansys=C:\Program Files\ANSYS Inc\v221\ansys\bin\winx64\MAPDL.exe
+
+
+SET path=ANALISES_CILAMCE\EP_CRE_CG_D1_4RE_3D
+SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
+SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_3D_DUPLO_2023.txt
+SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
+"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
+
+SET path=ANALISES_CILAMCE\EP_CRE_CG_D1_16RE_3D
+SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
+SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_3D_DUPLO_2023.txt
+SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
+"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
+
+SET path=ANALISES_CILAMCE\EP_CRE_ESP003_CG_D1_4RE_3D
+SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
+SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_3D_DUPLO_2023.txt
+SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
+"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
+
+SET path=ANALISES_CILAMCE\EP_CRE_ESP003_CG_D1_16RE_3D
+SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
+SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_3D_DUPLO_2023.txt
+SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
+"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
+
+SET path=ANALISES_CILAMCE\EP_CRE_ESP003_SG_D1_4RE_3D
+SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
+SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_3D_DUPLO_2023.txt
+SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
+"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
+
+SET path=ANALISES_CILAMCE\EP_CRE_ESP003_SG_D1_16RE_3D
+SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
+SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_3D_DUPLO_2023.txt
+SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
+"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
+
+SET path=ANALISES_CILAMCE\EP_CRE_ESP003_SG_D1_INF_AXI
+SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
+SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_AXI_2023.txt
+SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
+"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
+
+SET path=ANALISES_CILAMCE\EP_CRE_SG_D1_4RE_3D
+SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
+SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_3D_DUPLO_2023.txt
+SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
+"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
+
+SET path=ANALISES_CILAMCE\EP_CRE_SG_D1_16RE_3D
+SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
+SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_3D_DUPLO_2023.txt
+SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
+"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
+
+SET path=ANALISES_CILAMCE\EP_CRE_SG_D1_INF_AXI
+SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
+SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_AXI_2023.txt
+SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
+"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
+
+
+
