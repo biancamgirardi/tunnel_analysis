fd2823a75d1225a5885a7ea21f8dffa842056002
Bianca <eng.biancagirardi@gmail.com>
2024-08-09 21:28:29 -0300

    Update bat

--- ANALYSIS/ANALISES_01/bat_para_rodar_secao.bat
+++ ANALYSIS/ANALISES_01/bat_para_rodar_secao.bat
@@ -1,65 +1,58 @@
-SET pasta_ansys=C:\Program Files\ANSYS Inc\v221\ansys\bin\winx64\MAPDL.exe
+SET pasta_ansys="C:\Program Files\ANSYS Inc\v212\ansys\bin\winx64\MAPDL.exe"
+:: tirar as aspas de pasta_analise!
+SET pasta_analise=C:\gitprojects\tunnel_analysis\ANALYSIS\ANALISES_01
+SET np=16
 
+SET analise=GUO_2021\AXI_EL_SR
+SET workdirectory=%pasta_analise%\%analise%
+SET jobname=%pasta_analise%\%analise%\APDL_AXI_2024_01.txt
+SET output=%pasta_analise%\%analise%\file.out
+%pasta_ansys% -p ansys -smp -np %np% -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output%
 
-SET path=ANALISES_CILAMCE\EP_CRE_CG_D1_4RE_3D
-SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
-SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_3D_DUPLO_2023.txt
-SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
-"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
+SET analise=GUO_2021\EL_SR_D1_3RE
+SET workdirectory=%pasta_analise%\%analise%
+SET jobname=%pasta_analise%\%analise%\APDL_TWIN_2024_01.txt
+SET output=%pasta_analise%\%analise%\file.out
+%pasta_ansys% -p ansys -smp -np %np% -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output%
 
-SET path=ANALISES_CILAMCE\EP_CRE_CG_D1_16RE_3D
-SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
-SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_3D_DUPLO_2023.txt
-SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
-"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
+SET analise=GUO_2021\EL_SR_D1_6RE
+SET workdirectory=%pasta_analise%\%analise%
+SET jobname=%pasta_analise%\%analise%\APDL_TWIN_2024_01.txt
+SET output=%pasta_analise%\%analise%\file.out
+%pasta_ansys% -p ansys -smp -np %np% -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output%
 
-SET path=ANALISES_CILAMCE\EP_CRE_ESP003_CG_D1_4RE_3D
-SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
-SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_3D_DUPLO_2023.txt
-SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
-"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
+SET analise=MA_2020\AXI_EL_SR
+SET workdirectory=%pasta_analise%\%analise%
+SET jobname=%pasta_analise%\%analise%\APDL_AXI_2024_01.txt
+SET output=%pasta_analise%\%analise%\file.out
+%pasta_ansys% -p ansys -smp -np %np% -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output%
 
-SET path=ANALISES_CILAMCE\EP_CRE_ESP003_CG_D1_16RE_3D
-SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
-SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_3D_DUPLO_2023.txt
-SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
-"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
+SET analise=MA_2020\AXI_EP_DPII_SR
+SET workdirectory=%pasta_analise%\%analise%
+SET jobname=%pasta_analise%\%analise%\APDL_AXI_2024_01.txt
+SET output=%pasta_analise%\%analise%\file.out
+%pasta_ansys% -p ansys -smp -np %np% -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output%
 
-SET path=ANALISES_CILAMCE\EP_CRE_ESP003_SG_D1_4RE_3D
-SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
-SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_3D_DUPLO_2023.txt
-SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
-"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
+SET analise=MA_2020\AXI_EP_MC_SR
+SET workdirectory=%pasta_analise%\%analise%
+SET jobname=%pasta_analise%\%analise%\APDL_AXI_2024_01.txt
+SET output=%pasta_analise%\%analise%\file.out
+%pasta_ansys% -p ansys -smp -np %np% -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output%
 
-SET path=ANALISES_CILAMCE\EP_CRE_ESP003_SG_D1_16RE_3D
-SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
-SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_3D_DUPLO_2023.txt
-SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
-"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
+SET analise=MA_2020\EL_SR
+SET workdirectory=%pasta_analise%\%analise%
+SET jobname=%pasta_analise%\%analise%\APDL_TWIN_2024_01.txt
+SET output=%pasta_analise%\%analise%\file.out
+%pasta_ansys% -p ansys -smp -np %np% -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output%
 
-SET path=ANALISES_CILAMCE\EP_CRE_ESP003_SG_D1_INF_AXI
-SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
-SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_AXI_2023.txt
-SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
-"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
+SET analise=MA_2020\EP_DPII_SR
+SET workdirectory=%pasta_analise%\%analise%
+SET jobname=%pasta_analise%\%analise%\APDL_TWIN_2024_01.txt
+SET output=%pasta_analise%\%analise%\file.out
+%pasta_ansys% -p ansys -smp -np %np% -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output%
 
-SET path=ANALISES_CILAMCE\EP_CRE_SG_D1_4RE_3D
-SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
-SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_3D_DUPLO_2023.txt
-SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
-"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
-
-SET path=ANALISES_CILAMCE\EP_CRE_SG_D1_16RE_3D
-SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
-SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_3D_DUPLO_2023.txt
-SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
-"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
-
-SET path=ANALISES_CILAMCE\EP_CRE_SG_D1_INF_AXI
-SET workdirectory=D:\ALUNOS\FELIPE_QUEVEDO\%path%
-SET jobname=D:\ALUNOS\FELIPE_QUEVEDO\%path%\Script_AXI_2023.txt
-SET output=D:\ALUNOS\FELIPE_QUEVEDO\%path%\file.out
-"%pasta_ansys%" -p ansys -smp -np 2 -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output% 
-
-
-
+SET analise=MA_2020\EP_MC_SR
+SET workdirectory=%pasta_analise%\%analise%
+SET jobname=%pasta_analise%\%analise%\APDL_TWIN_2024_01.txt
+SET output=%pasta_analise%\%analise%\file.out
+%pasta_ansys% -p ansys -smp -np %np% -lch -dir %workdirectory% -j "file" -s read -l en-us -b -i %jobname% -o %output%
